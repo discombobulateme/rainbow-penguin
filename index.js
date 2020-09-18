8cda27009c1e47d75c766da869b1085568614012
discombobulateme <p@discombobulate.me>
2020-09-18 20:17:04 +0200

    Improve code

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-const colors = require("colors");
-const messages = require("./messages.json");
+const colors = require('colors');
+
+const messages = require('./messages.json');
 
 function rainbowPenguin() {
   let min = 5;
   let max = 30;
 
-  //Generate Random number between 5 and 200 seconds
-  let randomTime = Math.floor(
-    (Math.random() * (max - min + 1) + min) * 1000 * 60
-  );
-  // Randomize message
-  const randomMessages = messages[Math.floor(Math.random() * messages.length)];
-  console.log(colors.rainbow(`${randomMessages}`));
-  // scope.setTimeout(code[, delay]);
+  const randomMessage = messages[Math.floor(Math.random() * messages.length)];
+
+  console.log(colors.rainbow(`${randomMessage}`));
+
+  let randomTime = Math.floor((Math.random() * (max - min + 1) + min) * 1000 * 60);
+
   setTimeout(rainbowPenguin, randomTime);
 }
-
-rainbowPenguin();
 
 module.exports = rainbowPenguin;
