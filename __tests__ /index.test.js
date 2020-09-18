9da7eebba608adfe4f57c924d9c45f4786ab0acb
discombobulateme <p@discombobulate.me>
2020-09-18 20:16:47 +0200

    Fix tests

--- __tests__ /index.test.js
+++ __tests__ /index.test.js
@@ -1,30 +1,20 @@
-const rainbowPenguin = require('../index');
+const rainbowPenguin = require('../');
+const mockConsole = require('jest-mock-console');
+jest.mock('../messages.json');
 jest.useFakeTimers();
 
-// test random number
-// expect.any(constructor) for Math.random
-test('randocall calls its callback with a number', () => {
-  const mock = jest.fn();
-  randomTime(mock);
-  expect(mock).toBeCalledWith(expect.any(Number));
+it('should log a message on every timer tick', () => {
+  const restoreConsole = mockConsole();
+  rainbowPenguin();
+  jest.runOnlyPendingTimers();
+  jest.runOnlyPendingTimers();
+  expect(console.log).toHaveBeenCalledTimes(3);
+  restoreConsole();
 });
 
-// // tests setTimeOut
-// test("waits timeOut interval time to call the function", () => {
-//   rainbowPenguin();
-
-//   expect(jest.fn()).toHaveBeenCalledTimes(1);
-//   expect(jest.fn()).toHaveBeenLastCalled(expect.any(Function), 1000);
-// });
-
-// // test console.log messages
-// describe("log a message with colors"),
-//   () => {
-//     // define input
-//     test("it should log a message", () => {
-//       const expectedOutput = "Hi hi";
-//       const actualOutput = "No no";
-//       // define output
-//       expect(actualOutput).toBe(expectedOutput);
-//     });
-//   };
+it('should log the test message', () => {
+  const restoreConsole = mockConsole();
+  rainbowPenguin();
+  expect(console.log).toHaveBeenCalledWith('test message');
+  restoreConsole();
+});
