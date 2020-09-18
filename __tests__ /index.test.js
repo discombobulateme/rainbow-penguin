f6a0c951550accc37485cb39606e21a33559a8fb
discombobulateme <p@discombobulate.me>
2020-09-18 16:39:41 +0200

    Add testing files

--- __tests__ /index.test.js
+++ __tests__ /index.test.js
@@ -1,1 +1,30 @@
-describe("setTime out function");
+const rainbowPenguin = require("../index");
+jest.useFakeTimers();
+
+// test random number
+// expect.any(constructor) for Math.random
+test("randocall calls its callback with a number", () => {
+  const mock = jest.fn();
+  randomTime(mock);
+  expect(mock).toBeCalledWith(expect.any(Number));
+});
+
+// // tests setTimeOut
+// test("waits timeOut interval time to call the function", () => {
+//   rainbowPenguin();
+
+//   expect(jest.fn()).toHaveBeenCalledTimes(1);
+//   expect(jest.fn()).toHaveBeenLastCalled(expect.any(Function), 1000);
+// });
+
+// // test console.log messages
+// describe("log a message with colors"),
+//   () => {
+//     // define input
+//     test("it should log a message", () => {
+//       const expectedOutput = "Hi hi";
+//       const actualOutput = "No no";
+//       // define output
+//       expect(actualOutput).toBe(expectedOutput);
+//     });
+//   };
