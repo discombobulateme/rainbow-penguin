7463407f995f6e43bed7898675ea1d03eebfd0c0
discombobulateme <p@discombobulate.me>
2020-09-18 21:05:18 +0200

    Add 🌈 🐧: before messages

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ function rainbowPenguin() {
   const min = 5;
   const max = 25;
 
-  const randomMessage = messages[Math.floor(Math.random() * messages.length)];
+  const randomMessage = '🌈 🐧: ' + messages[Math.floor(Math.random() * messages.length)];
 
-  const asterisks = '*'.repeat(randomMessage.length).rainbow;
-  console.log(`${asterisks}\n${randomMessage.bold}\n${asterisks}`);
+  const asterisks = colors.rainbow('*'.repeat(randomMessage.length));
+  console.log(`${asterisks}\n${colors.bold(randomMessage)}\n${asterisks}`);
 
   const randomTime = Math.floor((Math.random() * (max - min + 1) + min) * 1000 * 60);
 
