caa19d4a7a7e5cf8d527bf3b4d3de0716ac27fe9
discombobulateme <p@discombobulate.me>
2020-09-18 20:41:51 +0200

    Fix time to minutes

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ colors.enable();
 const messages = require('./messages.json');
 
 function rainbowPenguin() {
-  const min = 2;
-  const max = 5;
+  const min = 5;
+  const max = 25;
 
   const randomMessage = messages[Math.floor(Math.random() * messages.length)];
 
   const asterisks = '*'.repeat(randomMessage.length).rainbow;
   console.log(`${asterisks}\n${randomMessage.bold}\n${asterisks}`);
 
-  const randomTime = Math.floor((Math.random() * (max - min + 1) + min) * 1000);
+  const randomTime = Math.floor((Math.random() * (max - min + 1) + min) * 1000 * 60);
 
   setTimeout(rainbowPenguin, randomTime);
 }
