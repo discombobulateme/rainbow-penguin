ef8e6dad2b29d261e465ea7d4ab3648456a13863
discombobulateme <p@discombobulate.me>
2020-09-18 16:40:02 +0200

    Add random time and random messages

--- index.js
+++ index.js
@@ -6,12 +6,16 @@ function rainbowPenguin() {
   let max = 30;
 
   //Generate Random number between 5 and 200 seconds
-  let rand = Math.floor((Math.random() * (max - min + 1) + min) * 1000 * 60);
+  let randomTime = Math.floor(
+    (Math.random() * (max - min + 1) + min) * 1000 * 60
+  );
   // Randomize message
   const randomMessages = messages[Math.floor(Math.random() * messages.length)];
   console.log(colors.rainbow(`${randomMessages}`));
-
-  setTimeout(rainbowPenguin, rand);
+  // scope.setTimeout(code[, delay]);
+  setTimeout(rainbowPenguin, randomTime);
 }
 
 rainbowPenguin();
+
+module.exports = rainbowPenguin;
